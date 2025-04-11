ec7334603ee027cc77018e36f5efde11581c5982
Iván Barcia <ivan@barcia.dev>
2025-04-11 19:06:02 +0200

    Remove eslint import plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import importPlugin from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import unicorn from "eslint-plugin-unicorn";
@@ -15,7 +14,6 @@ export default tseslint.config(
 			js.configs.recommended,
 			...tseslint.configs.strictTypeChecked,
 			...tseslint.configs.stylisticTypeChecked,
-			importPlugin.flatConfigs.recommended,
 			jsxA11y.flatConfigs.strict,
 			unicorn.configs.recommended,
 			...pluginQuery.configs['flat/recommended']
@@ -33,7 +31,6 @@ export default tseslint.config(
 		plugins: {
 			"react-hooks": reactHooks,
 			"react-refresh": reactRefresh,
-			importPlugin,
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
@@ -42,18 +39,6 @@ export default tseslint.config(
 				{ allowConstantExport: true },
 			],
 			"unicorn/prevent-abbreviations": "off",
-		},
-		settings: {
-			"import/resolver": {
-				typescript: {
-					project: "./tsconfig.json",
-
-					alias: {
-						find: "@",
-						replacement: "./src",
-					},
-				},
-			},
 		},
 	},
 );
